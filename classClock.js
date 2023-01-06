126480b536109401187d2fcac4f380c3223d5c93
KingdomKlyuch <kneedy93@gmail.com>
2023-01-06 16:54:58 +0200

    Add function to retrieve current day

--- classClock.js
+++ classClock.js
@@ -7,20 +7,28 @@ class Clock {
 
   setDate() {
     const now = new Date();
+  
 
+    
+    const day = now.getDate();
+    
+    const dayElement = document.querySelector('.day');
+    
+    dayElement.textContent = day;
+  
     const seconds = now.getSeconds();
     const secondsDegrees = ((seconds / 60) * 360) + 90;
     this.secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
-
+  
     const mins = now.getMinutes();
     const minsDegrees = ((mins / 60) * 360) + 90;
     this.minuteHand.style.transform = `rotate(${minsDegrees}deg)`;
-
+  
     const hours = now.getHours();
     const hoursDegrees = ((hours / 12) * 360) + 90;
     this.hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
-  }
-}
+  }}
+  
 
 const clock = new Clock();
 
